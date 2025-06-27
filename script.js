44de7e18abf2eb99b2297312f5463bbc10004bea
DPnimo11 <dpnimo11@gmail.com>
2025-06-27 16:15:09 -0400

    initial calculator design

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+function sub(n1, n2) {
+    return n1 - n2;
+}
+
+function mult(n1, n2) {
+    return n1 * n2;
+}
+
+function divide(n1, n2) {
+    return n1 / n2;
+}
+
+let num1 = undefined;
+let num2 = undefined;
+let op = undefined;
